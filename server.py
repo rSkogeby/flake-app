9b8b602f88e9f8a90aa2c9bcc132c328e57c2536
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-18 20:21:29 +0000

    Add barebone CRUD functions

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ session = DBSession()
 
 
 @app.route('/restaurant/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id):
+def restaurant_menu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
@@ -26,6 +26,21 @@ def restaurantMenu(restaurant_id):
         output += '<br />'
         output += '</p>'
     return output
+
+
+# Create route for newMenuItem function 
+def new_menu_item(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+# Create route for editMenuItem function 
+def edit_menu_item(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+# Create a route for deleteMenuItem function
+def delete_menu_item(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 
 if __name__ == "__main__":
