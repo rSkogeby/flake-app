063f44a8f231a2a84fd01df73098c7a47cae5632
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-18 20:06:12 +0000

    List restaurants from db

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from 
 from flask import Flask
 app = Flask(__name__) 
 
+from db_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
+@app.route('/hello')
 def here():
-    return 'here'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for item in items:
+        output += item.name
+        output += '<br />'
+    return output
 
 
 if __name__ == "__main__":
