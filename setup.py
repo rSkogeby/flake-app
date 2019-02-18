9c108ef7ee8d31d3bed4044eba5a1753075b751b
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-18 08:41:04 +0000

    Add basic web server functionality

---
+++ setup.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__) 
+
+
+@app.route('/')
+def here():
+    return 'here'
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
