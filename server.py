60eee7e8984dedcd6959076be28764e21252fc67
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-19 17:54:54 +0000

    Add GET/POST methods to newMenuItem

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) 
 
 from db_setup import Base, Restaurant, MenuItem
@@ -14,22 +14,12 @@ session = DBSession()
 @app.route('/restaurant/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for item in items:
-        output += '<p>'
-        output += item.name
-        output += '<br />'
-        output += item.description
-        output += '<br />'
-        output += item.price
-        output += '<br />'
-        output += '</p>'
-    return output
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 # Create route for newMenuItem function
-@app.route('/restaurant/<int:restaurant_id>/create/')
+@app.route('/restaurant/<int:restaurant_id>/create/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     return "page to create a new menu item. Task 1 complete!"
 
