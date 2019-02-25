d7370741f614d38c0206cedc45e4a3b747ecb97b
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-25 20:41:22 +0000

    Add imports for OAuth

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask import Flask, render_template, request, redirect,\
                   url_for, flash, jsonify
+from flask import session as login_session
+import random, string
 from werkzeug.datastructures import ImmutableMultiDict
 import copy
 
