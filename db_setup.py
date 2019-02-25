05af99f1eaa508257eb719265cb77155a76a7093
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-25 19:29:18 +0000

    Add serialise to restaurant table

--- db_setup.py
+++ db_setup.py
@@ -16,6 +16,13 @@ class Restaurant(Base):
     id = Column(
         Integer, primary_key = True
     )
+    @property
+    def serialize(self):
+        # Return object data in easily serialisable format
+        return {
+            'name': self.name,
+            'id': self.id
+        }
 
 
 class MenuItem(Base):
