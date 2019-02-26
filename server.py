85111fe42f17ad6c1185417dfcf6c7337748fa0e
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-26 21:25:20 +0000

    Add 400 response in gdisconnect in else statement

--- server.py
+++ server.py
@@ -144,9 +144,14 @@ def gdisconnect():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-    response = make_response(json.dumps('Successfully disconnected.'), 200)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 @app.route('/')
